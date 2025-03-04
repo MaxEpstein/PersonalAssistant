b6a261cbc15c7e741f44a7f7c7ff1d92e754693f
MaxEpstein <max.epstein@hotmail.com>
2025-03-04 12:59:36 -0500

    Added pip requirements file

--- spotifyControl.py
+++ spotifyControl.py
@@ -8,7 +8,13 @@ load_dotenv()
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv("SPOTIFY_CLIENT_ID"),
                                                client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
                                                redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
-                                               scope="user-modify-playback-state user-read-currently-playing playlist-modify-private playlist-modify-public user-library-modify"))
+                                               scope="user-modify-playback-state "
+                                               "user-read-currently-playing "
+                                               "playlist-modify-private "
+                                               "playlist-modify-public "
+                                               "user-library-modify "
+                                               "app-remote-control "
+                                               "streaming"))
 def pausePlay(query):
     if ("play" in query):
         print("Playing Spotify")
@@ -29,7 +35,6 @@ def volume(query):
     if (volume.endswith('%')):
         volume = volume[:-1]
     try:
-        print(volume)
         sp.volume(int(volume))
         print("setting spotify volume to " + volume)
         speak("setting Spot-tea-Phi volume to " + volume)
